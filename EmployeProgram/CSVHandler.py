a92cba10111e3046bfe3b0ada66cb50a32782450
Grant Smith <71993583+bomono3@users.noreply.github.com>
2024-03-11 04:45:06 -0500

    Added functionality to list employee, add employee, and remove employee

--- EmployeProgram/CSVHandler.py
+++ EmployeProgram/CSVHandler.py
@@ -2,11 +2,41 @@ import csv
 import os.path
 
 class csvHandling():
-    def createFile(self, name):
+    writer = None
+    reader = None
+    file1 = None
+    file2 = None
+    name = None
+    def __init__(self, name):
+        self.name = name
         path = './' + name + '.csv'
         check_file = os.path.isfile(path)
         if check_file:
+            self.file1 = open(name + '.csv', 'a', newline='')
+            self.writer = csv.writer(self.file1)
+            self.file2 = open(name + '.csv', 'r')
+            self.reader = csv.reader(self.file2)
             return
-        with open(name + '.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-
+        self.file1 = open(name + '.csv', 'w', newline='')
+        self.writer = csv.writer(self.file1)
+        self.file2 = open(name + '.csv', 'r')
+        self.reader = csv.reader(self.file2)
+    def readFile(self, name):
+        for row in self.reader:
+            print(row)
+    def AddEmployee(self, employee):
+        new_id = sum(1 for line in self.file2)
+        employee.insert(0, new_id)
+        self.writer.writerow(employee)
+    def removeEmployee(self, id):
+        with open('temp.csv', 'w', newline='') as out:
+            temp_writer = csv.writer(out)
+            for row in self.reader:
+                if row[0] != f"{id}":
+                    temp_writer.writerow(row)
+        self.file1.close()
+        self.file2.close()
+        os.remove(self.name + '.csv')
+        os.rename("temp.csv", self.name + '.csv')
+        self.__init__(self.name)
+        return
--- EmployeProgram/SystemHandler.py
+++ EmployeProgram/SystemHandler.py
@@ -5,13 +5,38 @@ class SystemHandling():
     
     gui_display = Gui.GuiDisplay()
     gui_input = Gui.GuiInputHandling()
-    csv_handler = CSVHandler.csvHandling()
+    csv_handler = None
     options = ['1. List Employee Info', '2. Add New Employee', '3. Update Employee Information', '4. Remove Employees', '5. Exit']
+    file_name = 'employees'
     
     def systemNavigation(self):
-        self.csv_handler.createFile('employees')
+        self.csv_handler = CSVHandler.csvHandling(self.file_name)
         self.gui_display.list_options(self.options)
         choice = self.gui_input.take_input("int5")
+        valid_input = 'false'
+        while valid_input == 'false':
+            match choice:
+                case '1':
+                    self.csv_handler.readFile(self.file_name)
+                    valid_input = 'true'
+                case '2':
+                    employee = self.gui_input.create_employee()
+                    self.csv_handler.AddEmployee(employee)
+                    valid_input = 'true'
+                case '3':
+                    print('case 3')
+                    valid_input = 'true'
+                case '4':
+                    id = self.gui_input.remove_employee()
+                    self.csv_handler.removeEmployee(id)
+                    valid_input = 'true'
+                case '5':
+                    valid_input = 'true'
+                    return
+                case _:
+                    print('input invalid, try again')
+            
+        
         
         
         
