2be050ef0212263ac8c2f22eda25da6cb18500b6
Grant Smith <71993583+bomono3@users.noreply.github.com>
2024-03-11 06:55:27 -0500

    added functionality to all DRUD operations

--- EmployeProgram/CSVHandler.py
+++ EmployeProgram/CSVHandler.py
@@ -24,19 +24,44 @@ class csvHandling():
     def readFile(self, name):
         for row in self.reader:
             print(row)
-    def AddEmployee(self, employee):
-        new_id = sum(1 for line in self.file2)
-        employee.insert(0, new_id)
+            self.__init__(self.name)
+    def addEmployee(self, employee):
+        new_id = max(self.reader, key=lambda row: int(row[0]))
+        new_id_number = int(new_id[0])
+        new_id_number += 1
+        new_id_number = str(new_id_number)
+        employee.insert(0, new_id_number)
         self.writer.writerow(employee)
+        self.__init__(self.name)
     def removeEmployee(self, id):
         with open('temp.csv', 'w', newline='') as out:
             temp_writer = csv.writer(out)
-            for row in self.reader:
-                if row[0] != f"{id}":
-                    temp_writer.writerow(row)
+            temp_list = [row for row in self.reader if row[0] != f"{id}"]
+            #for row in self.reader:
+            #    if row[0] != f"{id}":
+            #        temp_writer.writerow(row)
+            temp_writer.writerows(temp_list)
         self.file1.close()
         self.file2.close()
         os.remove(self.name + '.csv')
         os.rename("temp.csv", self.name + '.csv')
         self.__init__(self.name)
         return
+    def getEmployee(self, id):
+        employee = [row for row in self.reader if row[0] == f"{id}"]
+        self.file1.close()
+        self.file2.close()
+        self.__init__(self.name)
+        return employee[0]
+    def updateEmployee(self, id, employee):
+        with open('temp.csv', 'w', newline='') as out:
+            temp_writer = csv.writer(out)
+            temp_list = [(employee if row[0] == f"{id}" else row) for row in self.reader]
+            temp_writer.writerows(temp_list)
+        self.file1.close()
+        self.file2.close()
+        os.remove(self.name + '.csv')
+        os.rename("temp.csv", self.name + '.csv')
+        self.__init__(self.name)
+        return
+    
